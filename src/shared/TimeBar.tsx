f68e53fc374d5754e9ce9b16327a6ac294e87297
anovicenko74 <60508206+anovicenko74@users.noreply.github.com>
2023-06-11 02:14:48 +0200

    add important comment

--- src/shared/TimeBar.tsx
+++ src/shared/TimeBar.tsx
@@ -12,6 +12,7 @@ const TimeBar = ({ time, onTimer: handleSwitch, issueIndex }: Props) => {
     const [debounceIndex, setDebounceIndex] = useState(issueIndex)
     const timerId = useRef<NodeJS.Timer | null>(null)
 
+    // (2) когда страница обновилась сбрасываем таймер
     if (issueIndex !== debounceIndex) {
         setDebounceIndex(issueIndex)
         setTimer(0)
@@ -20,9 +21,10 @@ const TimeBar = ({ time, onTimer: handleSwitch, issueIndex }: Props) => {
     useEffect(() => {
         timerId.current = setTimeout(() => setTimer((tp) => ++tp), 1000)
 
+        // (1) таймер заканчивает свою работу и обновляется страница
         if (timer >= time) {
             handleSwitch()
-        }
+        } // в dev режиме двойной рендеринг, поэтому это условие обязано находиться в эффекте
 
         return () => {
             if (timerId.current !== null) {
